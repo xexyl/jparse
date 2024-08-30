65826592af4dc9530cf767a86f9ef8bf547e830a
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-08-30 09:26:17 -0700

    Add build script for CodeQL
    I am unsure if this is sufficient as this is the first time I have used
    it. The problem with the CodeQL without extra steps is that we require
    the dbg and dyn_array facilities. As long as git clone, cd, make all and
    sudo make install work (like with test.yml) then this should work. If it
    does not then something else will have to be done.

---
+++ .github/workflows/buildscript.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# we have to clone certain repos for the build to succeed as we require their
+# header files and their libraries.
+
+# first we need the dbg api from the dbg repo at https://github.com/lcn2/dbg.
+git clone https://github.com/lcn2/dbg
+cd dbg && make clobber all
+sudo make install
+
+# next we need the dyn_array api from the dyn_array repo at
+# https://github.com/lcn2/dyn_array.
+git clone https://github.com/lcn2/dyn_array
+cd dyn_array && make clobber all
+sudo make install
+
