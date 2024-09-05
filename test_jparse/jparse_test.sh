393d8b8f3bd3ded2d6be8187be163e9738afe294
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2024-09-05 10:22:02 -0700

    Change date format in jparse_test.sh
    By habit and without thinking I put the date format in jparse_test.sh in
    the international way rather than the way the rest of the version
    strings are (the American Way (tm, maybe, but I don't know :-) ).
    Although I would rather it the international way the other version
    strings are fine as is and more than one date form (and more than one
    form of English) is (are) used so to at least make them the same format
    I have changed it back (not that it's any less confusing except when the
    day is > 12).
    
    Anyway, the version string is now: "1.0.5 04-09-2024" (the version
    number itself does not need to be changed as no functionality changed).

--- test_jparse/jparse_test.sh
+++ test_jparse/jparse_test.sh
@@ -43,7 +43,7 @@
 
 # setup
 #
-export JPARSE_TEST_VERSION="1.0.5 04-09-2024"
+export JPARSE_TEST_VERSION="1.0.5 09-04-2024"
 export CHK_TEST_FILE="./test_jparse/json_teststr.txt"
 export JPARSE="./jparse"
 export PRINT_TEST="./test_jparse/print_test"
